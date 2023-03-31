a1ed62509e4a71077d0e1f5bf58b69f4dac82d99
sirflyingv <andthenyouadd@gmail.com>
2023-03-31 22:08:05 +0300

    try

--- src/Views/formView.js
+++ src/Views/formView.js
@@ -50,11 +50,11 @@ export const renderForm = (watchedState) => {
     btnAdd.setAttribute('disabled', true);
     input.setAttribute('disabled', true);
     input.classList.remove('is-invalid');
-    feedbackWrapperEl.innerHTML = `
-    <p id="feedback"
-      class="feedback m-0 position-absolute small">${i18nInstance.t(
-        'feedbackAwaiting',
-      )}</p>`;
+    // feedbackWrapperEl.innerHTML = `
+    // <p id="feedback"
+    //   class="feedback m-0 position-absolute small">${i18nInstance.t(
+    //     'feedbackAwaiting',
+    //   )}</p>`;
     input.value = '';
   }
   if (watchedState.formState === 'invalid_rss') {
@@ -80,32 +80,32 @@ export const renderForm = (watchedState) => {
     input.value = '';
   }
   if (watchedState.formState === 'submitted') {
-    // This when rss file successfully downloaded
-    // btnAdd.removeAttribute('disabled');
-    // input.removeAttribute('disabled');
-    // input.classList.remove('is-invalid');
-    // feedbackWrapperEl.innerHTML = `
-    // <p id="feedback"
-    //   class="feedback m-0 position-absolute small text-success">${i18nInstance.t(
-    //     'feedbackSubmitted',
-    //   )}</p>`;
-    // input.value = '';
-    btnAdd.disabled = false;
-    input.disabled = false;
+    btnAdd.removeAttribute('disabled');
+    input.removeAttribute('disabled');
     input.classList.remove('is-invalid');
+    feedbackWrapperEl.innerHTML = `
+    <p id="feedback"
+      class="feedback m-0 position-absolute small text-success">${i18nInstance.t(
+        'feedbackSubmitted',
+      )}</p>`;
     input.value = '';
-    const feedbackEl = document.createElement('p');
-    feedbackEl.id = 'feedback';
-    feedbackEl.classList.add(
-      'feedback',
-      'm-0',
-      'position-absolute',
-      'small',
-      'text-success',
-    );
-    feedbackEl.textContent = i18nInstance.t('feedbackSubmitted');
-    feedbackWrapperEl.innerHTML = '';
-    feedbackWrapperEl.appendChild(feedbackEl);
+
+    // btnAdd.disabled = false;
+    // input.disabled = false;
+    // input.classList.remove('is-invalid');
+    // input.value = '';
+    // const feedbackEl = document.createElement('p');
+    // feedbackEl.id = 'feedback';
+    // feedbackEl.classList.add(
+    //   'feedback',
+    //   'm-0',
+    //   'position-absolute',
+    //   'small',
+    //   'text-success',
+    // );
+    // feedbackEl.textContent = i18nInstance.t('feedbackSubmitted');
+    // feedbackWrapperEl.innerHTML = '';
+    // feedbackWrapperEl.appendChild(feedbackEl);
   }
 };
 
