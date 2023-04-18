2573746fa1dc6c28e8c12bf5829248c2c3c76924
sirflyingv <andthenyouadd@gmail.com>
2023-04-18 17:03:38 +0300

    refactor

--- src/Views/modalView.js
+++ src/Views/modalView.js
@@ -1,18 +1,20 @@
 export default (watchedState, i18nInstance, elements) => {
   const {
-    modalTitle, modalBody, fullArticleButton, modalCloseButton, modalWindow, modalBackdrop,
-  } =
-    elements;
+    modalTitle,
+    modalBody,
+    fullArticleButton,
+    modalCloseButton,
+    modalWindow,
+    modalBackdrop,
+  } = elements;
 
-  const modalPost = watchedState.posts.find((post) => post.postId === watchedState.ui.modalPostId);
+  const modalPost = watchedState.posts.find(
+    (post) => post.postId === watchedState.ui.modalPostId,
+  );
 
   modalTitle.innerText = modalPost.title;
   modalBody.innerText = modalPost.description;
   fullArticleButton.href = modalPost.link;
-
-  // modalTitle.innerText = watchedState.ui.modalPost.title;
-  // modalBody.innerText = watchedState.ui.modalPost.description;
-  // fullArticleButton.href = watchedState.ui.modalPost.link;
   fullArticleButton.innerText = ` ${i18nInstance.t('modalReadFull')} `;
   modalCloseButton.innerText = ` ${i18nInstance.t('modalCloseButton')} `;
 
